59bf630fb9808528f209d757d42077794c49797c
Miguel Fernandez <miguel.andresff@gmail.com>
2017-08-29 16:09:55 -0400

    Creating function to obtaing data from the user

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Assignment from "The Unix Workbench" course
 
-echo "Welcome user, How many files are in the current directory?"
-read num_files_user
+echo "Welcome user"
+
+function obt_data { 
+
+	echo "How many files are in the current directory?"
+	read num_files_user
+}
 
 let files_num=$(ls -l | wc -l)-1 	#Saving the numbers of files in the directory.
 
@@ -18,5 +23,8 @@ function testing_user_guess {
 		echo "Too low!!!"
 	fi
 }
+
+obt_data
+testing_user_guess
 
 
