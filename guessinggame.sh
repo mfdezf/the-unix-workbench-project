a56aa7399301ed1daf8ddc49be0aa5a987325a0f
Miguel Fernandez <miguel.andresff@gmail.com>
2017-08-29 14:45:45 -0400

    Changing the variable of the user guess

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,7 +2,7 @@
 # Assignment from "The Unix Workbench" course
 
 echo "Welcome user, How many files are in the current directory?"
-read num_files
+read num_files_user
 
-let files_num=$(ls -l | wc -l)-1
+let files_num=$(ls -l | wc -l)-1 	#Saving the numbers of files in the directory.
 
